7b1cb040eb6eb0230fc15afc76acac6ce4193680
Mauffin <99658965+Mauffin@users.noreply.github.com>
2022-06-25 01:05:28 -0400

    feat:landing page parte 1 / 3

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,12 @@
 import Navbar from './components/Navbar';
+import Landing from './pages/Landing';
+
 
 function App() {
 	return (
 		<>
 			<Navbar />
+			<Landing/>
 		</>
 	);
 }
---
+++ Client/src/pages/Landing.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    < >
+    <div className=' mx-auto  h-screen bg-yellow-300'>
+      <div className='container mx-auto flex justify-center pt-52'>
+        <p className='text-center text-5xl font-semibold '>
+                            Put the <span className='text-center text-4xl text-blue-900 font-black'>Help Ukraine</span>❤
+                            <br></br>
+                            widget on your website!
+        </p>
+
+      
+      </div>
+    </div>
+    <div className='mx-auto h-screen'>
+
+    </div>
+    </>
+  )
+}
+
+export default Landing
