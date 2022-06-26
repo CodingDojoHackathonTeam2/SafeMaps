667fd565963027580f73f3a27215340044583762
Mauffin <99658965+Mauffin@users.noreply.github.com>
2022-06-26 06:45:15 -0400

    feat: add mid content

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from './components/Navbar';
 import Landing from './pages/Landing';
-
+import Map from './components/Map';
+import Footer from './components/Footer'
 
 function App() {
 	return (
 		<>
 			<Navbar />
 			<Landing/>
+			<Footer/>
 		</>
 	);
 }
---
+++ Client/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+
+    
+  return (
+    <>
+    
+      <div className='h-56'>
+        <div className='flex'>
+          
+        </div>
+      </div>
+    
+    </>
+    
+  )
+}
+
+export default Footer
